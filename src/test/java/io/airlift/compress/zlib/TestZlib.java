92c2652544c6c94bde4b3353a139dc8917d5a37a
David Phillips <david@acz.org>
2022-10-24 18:25:19 -0700

    Add TestZlib using JDK implementation

---
+++ src/test/java/io/airlift/compress/zlib/TestZlib.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.compress.zlib;
+
+import io.airlift.compress.AbstractTestCompression;
+import io.airlift.compress.Compressor;
+import io.airlift.compress.Decompressor;
+import io.airlift.compress.thirdparty.JdkDeflateCompressor;
+import io.airlift.compress.thirdparty.JdkInflateDecompressor;
+
+public class TestZlib
+        extends AbstractTestCompression
+{
+    @Override
+    protected Compressor getCompressor()
+    {
+        return new JdkDeflateCompressor();
+    }
+
+    @Override
+    protected Decompressor getDecompressor()
+    {
+        return new JdkInflateDecompressor();
+    }
+
+    @Override
+    protected Compressor getVerifyCompressor()
+    {
+        return new JdkDeflateCompressor();
+    }
+
+    @Override
+    protected Decompressor getVerifyDecompressor()
+    {
+        return new JdkInflateDecompressor();
+    }
+}
