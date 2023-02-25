1a2b4f94812444d749d48893d5e5fa8f8c5aa1b0
Dain Sundstrom <dain@iq80.com>
2023-02-25 14:01:28 -0800

    Add JdkDeflateCodec

---
+++ src/test/java/io/airlift/compress/deflate/TestJdkDeflateCodecByteAtATime.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.compress.deflate;
+
+import io.airlift.compress.AbstractTestCompression;
+import io.airlift.compress.Compressor;
+import io.airlift.compress.Decompressor;
+import io.airlift.compress.HadoopCodecCompressor;
+import io.airlift.compress.HadoopCodecDecompressor;
+import io.airlift.compress.HadoopCodecDecompressorByteAtATime;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.DefaultCodec;
+
+public class TestJdkDeflateCodecByteAtATime
+        extends AbstractTestCompression
+{
+    private final CompressionCodec verifyCodec;
+
+    public TestJdkDeflateCodecByteAtATime()
+    {
+        DefaultCodec codec = new DefaultCodec();
+        codec.setConf(new Configuration());
+        this.verifyCodec = codec;
+    }
+
+    @Override
+    protected boolean isByteBufferSupported()
+    {
+        return false;
+    }
+
+    @Override
+    protected Compressor getCompressor()
+    {
+        return new HadoopCodecCompressor(new JdkDeflateCodec(), new MockJdkDeflateCompressor());
+    }
+
+    @Override
+    protected Decompressor getDecompressor()
+    {
+        return new HadoopCodecDecompressorByteAtATime(new JdkDeflateCodec());
+    }
+
+    @Override
+    protected Compressor getVerifyCompressor()
+    {
+        return new HadoopCodecCompressor(verifyCodec, new MockJdkDeflateCompressor());
+    }
+
+    @Override
+    protected Decompressor getVerifyDecompressor()
+    {
+        return new HadoopCodecDecompressor(verifyCodec);
+    }
+}
