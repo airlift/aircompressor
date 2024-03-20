4f384afdea9b6d0109515e7e222b0e93a77109df
Dain Sundstrom <dain@iq80.com>
2024-03-19 22:52:10 -0700

    Replace test usages of javax.inject annotations with Guice

--- src/test/java/io/airlift/compress/TestingModule.java
+++ src/test/java/io/airlift/compress/TestingModule.java
@@ -16,10 +16,9 @@ package io.airlift.compress;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import io.airlift.compress.benchmark.DataSet;
 import org.openjdk.jmh.annotations.Param;
-
-import javax.inject.Singleton;
 
 import java.io.IOException;
 import java.util.ArrayList;
