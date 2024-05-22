da25345d6e104a9e6bb750784b0b9287bd04cbce
Dain Sundstrom <dain@iq80.com>
2024-05-21 21:12:46 -0700

    Verify ZstdCodec using ZstdJNI instead of Hadoop code
    The underlying format for zstd in Hadoop is the standard framed format.
    The Hadoop JNI code for Zstd corrupts the process symbol table on Linux
    by loading the Zstd library into the global process symbol table.

--- src/test/java/io/airlift/compress/zstd/TestZstdCodecByteAtATime.java
+++ src/test/java/io/airlift/compress/zstd/TestZstdCodecByteAtATime.java
@@ -18,9 +18,9 @@ import io.airlift.compress.AbstractTestCompression;
 import io.airlift.compress.Compressor;
 import io.airlift.compress.Decompressor;
 import io.airlift.compress.HadoopCodecCompressor;
-import io.airlift.compress.HadoopCodecDecompressor;
 import io.airlift.compress.HadoopCodecDecompressorByteAtATime;
-import io.airlift.compress.HadoopNative;
+import io.airlift.compress.thirdparty.ZstdJniCompressor;
+import io.airlift.compress.thirdparty.ZstdJniDecompressor;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.compress.CompressionCodec;
 import org.junit.jupiter.api.Test;
@@ -30,10 +30,6 @@ import java.io.IOException;
 class TestZstdCodecByteAtATime
         extends AbstractTestCompression
 {
-    static {
-        HadoopNative.requireHadoopNative();
-    }
-
     private final CompressionCodec verifyCodec;
 
     TestZstdCodecByteAtATime()
@@ -64,13 +60,15 @@ class TestZstdCodecByteAtATime
     @Override
     protected Compressor getVerifyCompressor()
     {
-        return new HadoopCodecCompressor(verifyCodec, new ZstdCompressor());
+        // Hadoop format is the standard Zstd framed format
+        return new ZstdJniCompressor(3);
     }
 
     @Override
     protected Decompressor getVerifyDecompressor()
     {
-        return new HadoopCodecDecompressor(verifyCodec);
+        // Hadoop format is the standard Zstd framed format
+        return new ZstdJniDecompressor();
     }
 
     @Test
